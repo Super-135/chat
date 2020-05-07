0c2add37bb73b0b7811db331807fd59a7e54370a
Super-135 <super05061982@gmail.com>
2020-05-07 12:20:49 +0300

    master

--- chat-client/src/main/java/Main.java
+++ chat-client/src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+
     }
 
 
